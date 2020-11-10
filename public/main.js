4200dd32bd08eda6f26c4c25c78bd4278aa1801a
Raphael Serota <raphael.serota@gmail.com>
2020-11-10 10:22:00 -0600

    sorting data on the front end

--- public/main.js
+++ public/main.js
@@ -1,14 +1,65 @@
 let vm = new Vue({
 	el: '#app',
+	data: {
+		trainLists: [], //  a list of ids for each train list (i.e. each CSV file that has been uploaded)
+		trainRuns: [], // the particular list of train runs that we're looking at currently
+		sortColumn: 'run_number',
+        sortDirection: 1,
+		currentPage: 1,
+		perPage: 5,
+
+	},
+	created: async function(){
+		console.log('hi')
+		let trainLists = await fetch('/train-list').then((data)=>{ return data.json() })
+		console.log('??', trainLists)
+		this.trainLists = trainLists.data
+
+	},
 	methods: {
-		uploadFile: async ()=>{
-			console.log('hi')
+		uploadFile: async function(){
 			let csvFile = document.getElementById("file-upload-input").files[0];
 			let formData = new FormData();
 
 			formData.append("train-list", csvFile);
-			let response = await fetch('/train-list', {method: "POST", body: formData});
-			let responseObj = await response.json()
-		}
+			let response = await fetch('/train-list', {method: "POST", body: formData}).then((data)=>{ return data.json()  })
+		},
+		getRuns: async function(trainList){
+			let trainRuns = await fetch(`/train-list/${trainList.id}`).then((data)=>{ return data.json() })
+			this.trainRuns = trainRuns.data
+
+		},
+		setPage: function(n){
+			
+			this.currentPage += n
+			if ( this.currentPage < 1 ) {
+				this.currentPage = 1
+			}
+		},
+		sortBy: function(col){
+			this.currentPage = 1
+            if ( this.sortColumn == col ) { this.sortDirection = -this.sortDirection }
+            else {
+                this.sortColumn = col
+                this.sortDirection = 1
+            }
+        },
+	},
+	computed: {
+		sortedRuns: function(){
+			let col = this.sortColumn
+			let dir = this.sortDirection
+			return this.trainRuns.slice().sort(function(a,b){
+				// default sort block
+				if      ( a[col] >  b[col] ) { return dir  }
+				else if ( a[col] <  b[col] ) { return -dir }
+				else if ( a[col] == b[col] ) { return 0    }
+			})
+        },
+		runsPage: function(){
+            return this.sortedRuns.filter((item, i)=>{
+                return (i >= (this.currentPage-1) * this.perPage) && (i < (this.currentPage) * this.perPage )
+            })
+        },
 	}
 })
--- server.js
+++ server.js
@@ -9,20 +9,41 @@ let upload = multer()
 app.use(express.static('./public'))
 app.use(express.json())
 
+app.get('/train-list', async (req, res)=>{
+	let trainLists = await pool.query(`
+		SELECT * FROM train_lists
+	`)
+
+	res.send({data:trainLists.rows})
+})
+
+app.get('/train-list/:id', async (req, res)=>{
+	let trainRuns = await pool.query(`
+		SELECT * FROM train_runs
+		WHERE train_list_id = $1
+	`, [req.params.id]
+	)
+
+	res.send({data:trainRuns.rows})
+})
 
 app.post('/train-list', upload.single('train-list'), async (req, res)=>{
 	console.log('new train list!')
 	console.log(req.file.buffer.toString())
 	let csvObj = papa.parse(req.file.buffer.toString())
 	console.log(csvObj)
+	csvObj.data.shift() // remove the headers from the CSV data
 
 	let trainListInsertion = await pool.query(`INSERT INTO train_lists DEFAULT VALUES RETURNING id`)
 	let trainListId = trainListInsertion.rows[0].id
 
-	let trainRunInsertion = await pool.query(`
-		INSERT INTO train_runs ( train_list_id )
-		VALUES                 ( $1 )
-	`, [trainListId])
+	for ( let run of csvObj.data ) {
+		let trainRunInsertion = await pool.query(`
+			INSERT INTO train_runs ( train_list_id, train_line, route_name, run_number, operator_id )
+			VALUES                 ( $1,            $2,         $3,         $4,         $5          )
+		`, [trainListId, run[0], run[1], run[2], run[3]]
+		)
+	}
 
 	res.send({got:'em'})
 })
